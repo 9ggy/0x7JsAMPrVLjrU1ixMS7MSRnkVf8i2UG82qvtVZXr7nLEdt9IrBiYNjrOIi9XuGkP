7fe8186aa14bee492ba7b1a3cd91854630b07b60
corruptdatabases <141536453+9ggy@users.noreply.github.com>
2024-11-12 18:17:10 -0600

    Create src.lua

---
+++ src.lua
@@ -0,0 +1,28 @@
+-- korblox
+if Script.Settings.Korblox then
+    local ply = game.Players.LocalPlayer
+    local chr = ply.Character
+    chr.RightLowerLeg.MeshId = "902942093"
+    chr.RightLowerLeg.Transparency = "1"
+    chr.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+    chr.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
+    chr.RightFoot.MeshId = "902942089"
+    chr.RightFoot.Transparency = "1"
+end
+-- Headless
+if Script.Settings.Headless then
+    game.Players.LocalPlayer.Character.Head.Transparency = 1
+    game.Players.LocalPlayer.Character.Head.Transparency = 1
+    f   or i,v in pairs(game.Players.LocalPlayer.Character.Head:GetChildren()) do
+    if (v:IsA("Decal")) then
+    v.Transparency = 1
+end
+end
+-- Zombie
+if Script.Settings.ZombieAnimations then
+    local Animate = game.Players.LocalPlayer.Character.Animate
+    Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+    Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+    game.Players.LocalPlayer.Character.Humanoid.Jump = false
+wait(1)
+end
