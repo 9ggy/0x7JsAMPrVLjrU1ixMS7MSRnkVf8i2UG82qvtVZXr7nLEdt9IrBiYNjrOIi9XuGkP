6cb596d8e7ee0cd79b6dd1f26dd9c9bded6d5a58
corruptdatabases <141536453+9ggy@users.noreply.github.com>
2024-11-12 18:37:02 -0600

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,5 +1,5 @@
 -- // Korblox Source
-if Script.Settings.Korblox then
+if Script.AvatarOptions.Korblox then
     local ply = game.Players.LocalPlayer
     local chr = ply.Character
     chr.RightLowerLeg.MeshId = "902942093"
@@ -11,7 +11,7 @@ if Script.Settings.Korblox then
 end
 
 -- // Headless Source
-if Script.Settings.Headless then
+if Script.AvatarOptions.Headless then
     game.Players.LocalPlayer.Character.Head.Transparency = 1
     game.Players.LocalPlayer.Character.Head.Transparency = 1
     for i,v in pairs(game.Players.LocalPlayer.Character.Head:GetChildren()) do
@@ -21,11 +21,74 @@ end
 end
         
 -- // Zombie
-if Script.Settings.ZombieAnimations then
+if Script.AvatarOptions.FakeAnimations then
     local Animate = game.Players.LocalPlayer.Character.Animate
     Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
     Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
-    game.Players.LocalPlayer.Character.Humanoid.Jump = false
+    Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=656117878"
+    Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+    game.Players.LocalPlayer.Character.Humanoid.Jump = true
 wait(1)
 end
 end
+
+-- // Da Hood Animations
+local AnimationIds = {
+    Lay = "rbxassetid://0",
+    Greet = "rbxassetid://0",
+}
+
+if game.PlaceId == 2788229376 or game.PlaceId == 2788229376 then -- // Da Hood
+    AnimationIds = {
+        Lay = "rbxassetid://3152378852",
+        Greet = "rbxassetid://3189777795",
+    }
+elseif game.PlaceId == 5602055394 or game.PlaceId == 7951883376 then
+    AnimationIds = {
+        Lay = "rbxassetid://12848619324",
+        Greet = "rbxassetid://12848108646",
+    }
+elseif game.PlaceId == 12867571492 then
+    AnimationIds = {
+        Lay = "rbxassetid://12649394546",
+        Greet = "rbxassetid://12649400354s",
+    }
+elseif game.PlaceId == 14412601883 then -- // Hood Bank
+    AnimationIds = {
+        Lay = "rbxassetid://14414332833",
+        Greet = "rbxassetid://14413912697",
+    }
+elseif game.PlaceId == 14413712255 then 
+    AnimationIds = {
+        Lay = "rbxassetid://14414368594",
+        Greet = "rbxassetid://14414355346",
+    }
+end
+    
+    
+game:GetService("UserInputService").InputBegan:Connect(function(key, gp)
+    if not gp then 
+        if key.KeyCode == Script.DaHood.LayKey then 
+            if Script.DaHood.Lay then 
+                local Lay = Instance.new("Animation")
+                Lay.AnimationId = AnimationIds.Lay
+                local LayTrack = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Lay)
+                LayTrack:Play()
+                game.Players.LocalPlayer.Character.Humanoid.Running:Connect(function()
+                    LayTrack:Stop()
+                end)
+            end
+        end
+        if key.KeyCode == Script.DaHood.GreetKey then
+            if Script.DaHood.Greet then 
+                local Greet = Instance.new("Animation")
+                Greet.AnimationId = AnimationIds.Greet
+                local GreetTrack = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Greet)
+                GreetTrack:Play()
+                game.Players.LocalPlayer.Character.Humanoid.Running:Connect(function()
+                    GreetTrack:Stop()
+                end)
+            end
+        end
+    end
+end)
